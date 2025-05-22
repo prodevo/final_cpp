01225444aa59d559237aa26a78af76facae96f4e
WhiteCat <93149057+prodevo@users.noreply.github.com>
2025-05-22 22:53:28 +0300

    creating marketplace.h

---
+++ marketplace.h
@@ -0,0 +1,29 @@
+#ifndef marketplace_h
+#define marketplace_h
+
+struct product {
+    int id;
+    const char* name;
+    float price;
+};
+
+struct cart_it {
+    product product;
+    int quantity;
+};
+
+struct cart {
+    cart_it* items;
+    int size;
+    int capacity;
+};
+
+int initialize_cart(cart* cart);
+int delete_cart(cart* cart);
+int show_products();
+int add_to_cart(cart* cart);
+int show_cart(const cart& cart);
+int save_cart(const cart& cart);
+int load_cart(cart* cart);
+
+#endif
